a04c62d007d7f5907122e8f4a42fdcf1fd990999
manish8621 <43578519+manish8621@users.noreply.github.com>
2021-05-17 07:54:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser"); 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    var result = String(n1/(n2*n2));
+    res.send("This is answer "+result);
+});
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+});
